2043d34f4e349a8121302c08f897b99c525847ef
Abhishek kumar <akumar4318@gmial.com>
2024-10-03 13:22:36 +0530

    .

--- SORTING/app.js
+++ SORTING/app.js
@@ -1,46 +1,46 @@
 
-//! BUBBLE SORTING
+// ! BUBBLE SORTING
 
-// function bubblesort(arr){
-// let n=arr.length
-//     for(let i=1;i<n;i++){
-//         for(let j=0;j<n-i;j++){
-//             if(arr[j]>arr[j+1]){
-//                 [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
+function bubblesort(arr){
+let n=arr.length
+    for(let i=1;i<n;i++){
+        for(let j=0;j<n-i;j++){
+            if(arr[j]>arr[j+1]){
+                [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
 
-//             }
+            }
            
-//         }
-//     }
-// return arr;
-// }
+        }
+    }
+return arr;
+}
 
-// console.log(bubblesort([14,54,2,33,12,9]))
+console.log(bubblesort([14,54,2,33,12,9]))
 
 
-//! SELECTION SORT--
+// ! SELECTION SORT--
 
-// function selectionsort(arr){
+function selectionsort(arr){
 
-//     let n=arr.length;
+    let n=arr.length;
 
-//     for(let i=0;i<n-1;i++){
+    for(let i=0;i<n-1;i++){
         
-//         let minvalue=i;
+        let minvalue=i;
 
-//         for(let j=i+1;j<n;j++){
-//             if(arr[j]<arr[minvalue]){
-//                 minvalue=j;
-//             }
+        for(let j=i+1;j<n;j++){
+            if(arr[j]<arr[minvalue]){
+                minvalue=j;
+            }
             
-//         }
+        }
 
-//         [arr[minvalue],arr[i]]=[arr[i],arr[minvalue]]
-//     }
-//  return arr;
-// }
+        [arr[minvalue],arr[i]]=[arr[i],arr[minvalue]]
+    }
+ return arr;
+}
 
-// console.log(selectionsort([14,54,12,13,13,12,9,45,44,43,42]))
+console.log(selectionsort([14,54,12,13,13,12,9,45,44,43,42]))
 
 //! INSERTION SORTING
 
