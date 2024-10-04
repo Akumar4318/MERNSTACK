d482d44663ae145cd7a0790f7d02f43955a4d74a
Abhishek kumar <akumar4318@gmial.com>
2024-10-04 15:57:17 +0530

    reduce and fillter

--- Day13/app.js
+++ Day13/app.js
@@ -1,24 +1,59 @@
 //! MAP IS HOF 
 //NOTE -  we can use index also by using map fn. 
 
-let arr=[1,2,3,4,5,6,7,8];
+// let arr=[-1,1,2,3,4,5,6,7,8];
 
 
-let narr=arr.map(function(item,index){
-    console.log(item,index)
-    return item*2 
+// let narr=arr.map(function(item,index){
+//     console.log(item,index)
+//     return item*2 
     
+// })
+// console.log(narr)
+
+// using arrow function
+
+// let narr =arr.map((item,index)=>{
+//     return item*2;
+// })
+
+// console.log(narr)
+
+
+
+
+//! FILTER ---
+// let arr=[-1,1,2,3,4,5,6,7,8];
+
+
+
+// let Narr=arr.filter((item,index)=>{
+
+//     // return item%2==0   // even 
+//     return item<0  
+//     return  
+// })
+
+// console.log(Narr)
+
+
+// let str="aman kumar";
+
+// let str1=str.filter((item)=>{
+//     return item=a,e,i,o,u;
+// })
+// console.log(str1)
+
+
+
+//! REDUCE
+
+let arr=[-1,1,2,3,4,5,6,7,8];
+
+ 
+let sum=arr.reduce((accumulator,item)=>{
+    return accumulator+item
+
 })
-console.log(narr)
 
-using arrow function
-let narr =arr.map((item,index)=>{
-    return item*2;
-})
-
-console.log(narr)
-
-
-
-
-//! FILLTER ---
+console.log(sum)
